450b9e3de31f9842209daed3c65440e6579cb2fd
Sanjan I S <sanjanis111@gmail.com>
2025-07-26 11:00:01 +0530

    Starting

---
+++ datatype.h
@@ -0,0 +1,11 @@
+typedef unsigned char uint8;
+typedef signed char int8;
+
+typedef unsigned short int uint16;
+typedef signed short int int16;
+
+typedef unsigned int uint32;
+typedef signed int int32;
+
+typedef float float32;
+typedef double f64;
